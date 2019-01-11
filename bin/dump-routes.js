31b4075377cf5d2e8414c569e8c719e445e5aad8
shun <shun@getto.systems>
2019-01-11 11:59:20 +0000

    rename: parse-routes -> dump-routes

--- bin/dump-routes.js
+++ bin/dump-routes.js
@@ -6,8 +6,8 @@ const commander = require("commander");
 const routes = require("../index.js");
 
 commander.version(require("../package.json").version)
-  .option("-f, --file [file]", "The file to parse that includes routes information")
+  .option("-f, --file [file]", "The file to dump that includes routes information")
   .option("-e, --encode [encode]", "The encode of routes file. Defaults to utf8", "utf8")
   .parse(process.argv);
 
-routes.parse(commander.file, commander.encode);
+routes.dump(commander.file, commander.encode);
